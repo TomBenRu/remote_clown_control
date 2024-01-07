7be0aa14309be6fc1b86c06e233dffd2407c8f76
tombe <mail@thomas-ruff.de>
2024-01-07 22:09:26 +0100

    Work in progress

---
+++ chat.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import asyncio
+import json
+
+import websockets
+
+
+async def hello():
+    uri = "ws://localhost:8000/ws/notifications/ "
+    cookie = 'ws-remote-client;Klient'
+    async with websockets.connect(uri, extra_headers={'Cookie': cookie}) as websocket:
+        name = input("What's your name? ")
+        data = json.dumps({'name': name})
+
+        await websocket.send(data)
+        print(f">>> {name}")
+
+        greeting = await websocket.recv()
+        print(f"<<< {greeting}")
+
+if __name__ == "__main__":
+    asyncio.run(hello())
